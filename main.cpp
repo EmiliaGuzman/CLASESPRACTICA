1c8f10cbf6b3516d440f509ef2c236c841007f9b
EmiliaGuzman <54367844+EmiliaGuzman@users.noreply.github.com>
2019-11-08 11:55:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -42,18 +42,89 @@ class Curso{
 private:
     string nombre;
     string codigo;
-    char* alumnos[];
+    char alumnos[4];
     int cantidadAlumnos();
+public:
     void getDatos();
-
-
+    void imprimirDatos();
 
 };
 
+void Curso::getDatos()
+{
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+    cout << "Ingrese su codigo: ";
+    cin >> codigo;
+    cout << "Ingrese la lista de alumnos: ";
+    cin >> alumnos[4];
+    for(int i=0; i<4; i++)
+    {
+        alumno[i];
+    }
+    cout << "Ingrese la cantidad de alumnos: ";
+    cin >> cantidadAlumnos;
+}
+
+void Curso::imprimirDatos()
+{
+    cout << "Su nombre es "<< nombre << " "<< apellido << endl;
+    cout << "Con codigo "<< codigo;
+    cout << endl;
+    cout << "La lista de alumnos es como sigue: "<< alumnos[4]<< endl;
+    cout << "La cantidad de alumnos"<< cantidadAlumnos[];
+    cout << endl;
+
+}
+
+};
+
+class Curso{
+private:
+    string nombre;
+    string codigo;
+    char alumnos[4];
+    int cantidadAlumnos();
+public:
+    void getDatos();
+    void imprimirDatos();
+
+};
+
+void Curso::getDatos()
+{
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+    cout << "Ingrese su codigo: ";
+    cin >> codigo;
+    cout << "Ingrese la lista de alumnos: ";
+    cin >> alumnos[4];
+    for(int i=0; i<4; i++)
+    {
+        alumno[i];
+    }
+    cout << "Ingrese la cantidad de alumnos: ";
+    cin >> cantidadAlumnos;
+}
+
+void Curso::imprimirDatos()
+{
+    cout << "Su nombre es "<< nombre << " "<< apellido << endl;
+    cout << "Con codigo "<< codigo;
+    cout << endl;
+    cout << "La lista de alumnos es como sigue: "<< alumnos[4]<< endl;
+    cout << "La cantidad de alumnos"<< cantidadAlumnos[];
+    cout << endl;
+
+}
 
 int main()
 {
     Estudiante ana;
     ana.getDatos();
     ana.imprimirDatos();
+    
+    Curso df;
+    df.getDatos();
+    df.imprimirDatos();
 }
